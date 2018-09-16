d8b920a587e0651347307577cdbe7f570cc96ad4
ymzk-jp <ymzk@gmail.com>
2018-09-16 19:48:18 +0900

    dummyデータ作成@part1

---
+++ dummy_datas/photos_dummy.sql
@@ -0,0 +1,12 @@
+INSERT INTO
+`photos`
+(`product_id`,`photo_url`)
+VALUES
+('1','https://makeit.com/s3/photo'),
+('2','https://makeit.com/s3/photo'),
+('3','https://makeit.com/s3/photo'),
+('4','https://makeit.com/s3/photo'),
+('1','https://makeit.com/s3/photo2'),
+('1','https://makeit.com/s3/photo3'),
+('1','https://makeit.com/s3/photo4')
+;
