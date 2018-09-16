b0b9ececc8badfcf2d9fe0573947a985213cfd15
ymzk-jp <ymzk@gmail.com>
2018-09-16 16:01:51 +0900

    ひとまずこれでダミーデータの作成に入る

--- V20180916/02_products.sql
+++ V20180916/02_products.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `products` (
-    `product_id` SMALLINT NOT NULL AUTO_INCREMENT COMMENT '作品番号',
-    `product_title` VARCHAR(50) NOT NULL DEFAULT "" COMMENT '作品名',
+    `id` SMALLINT NOT NULL AUTO_INCREMENT COMMENT '作品番号',
+    `title` VARCHAR(50) NOT NULL DEFAULT "" COMMENT '作品名',
     `concept` VARCHAR(250) NOT NULL DEFAULT "" COMMENT '作品コンセプト',
     `genre` VARCHAR(8) NOT NULL DEFAULT "" COMMENT 'fashionもしくはbeauty',
     `representative_student_id` SMALLINT NOT NULL COMMENT '代表者学生ID',
     `message` VARCHAR(250) NOT NULL DEFAULT "" COMMENT '学生から作品へのメッセージ',
     `update_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新日時',
-    PRIMARY KEY (`product_id`),
-    CONSTRAINT `students_to_student_id` FOREIGN KEY (`representative_student_id`) REFERENCES `students` (`student_id`)
+    PRIMARY KEY (`id`),
+    CONSTRAINT `students_to_id` FOREIGN KEY (`representative_student_id`) REFERENCES `students` (`id`)
 )DEFAULT CHARSET=utf8mb4 COMMENT='作品一覧';
--- V20180916/04_product_menbers.sql
+++ V20180916/04_product_menbers.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `product_menbers` (
     `product_id` SMALLINT NOT NULL COMMENT '作品番号',
     `student_id` SMALLINT NOT NULL COMMENT '学生ID',
-    CONSTRAINT `product_menbers_to_product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`) ON UPDATE CASCADE ON DELETE CASCADE,
-    CONSTRAINT `product_menbers_to_student_id` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`) ON UPDATE CASCADE ON DELETE CASCADE
+    CONSTRAINT `product_menbers_to_product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT `product_menbers_to_student_id` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
 )DEFAULT CHARSET=utf8mb4 COMMENT='チーム構成一覧';
