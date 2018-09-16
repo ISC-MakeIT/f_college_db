5c0a0635f2be683cf73abf4dfddc22fd94b1693d
ymzk-jp <ymzk@gmail.com>
2018-09-16 16:56:47 +0900

    ダミーデータ@students

---
+++ dummy_datas/products_dummy.sql
@@ -0,0 +1,5 @@
+INSERT INTO
+`products`
+(`title`,`concept`,`genre`,`representative_student_id`,`message`)
+VALUES
+('情熱と情熱の情熱','３日で作りました','fashion','1'',''),
---
+++ dummy_datas/students_dumm.sql
@@ -0,0 +1,17 @@
+INSERT INTO 
+`students` 
+(`name`,`grade`,`major`,`message`,`profile_photo_url`) 
+VALUES 
+('雉谷沙也加','2年','ファッション科','がんばります','https://makeit.com/s3/photo'),
+('猿飛明日香','2年','ファッション科','がんばってます','https://makeit.com/s3/photo'),
+('大犬楓','2年','ファッション科','がんばりおん','https://makeit.com/s3/photo'),
+('蝶野彩芽','2年','ファッション科','優勝したい','https://makeit.com/s3/photo'),
+('絛優樹菜','2年','ファッション科','優勝します','https://makeit.com/s3/photo'),
+('蟻塚椿','2年','ファッション科','有償保証','https://makeit.com/s3/photo'),
+('東雲あずさ','2年','ビューティー科','全力で取り組みたいです','https://makeit.com/s3/photo'),
+('紫雲秋菜','2年','ビューティー科','全力をぶつけています','https://makeit.com/s3/photo'),
+('三雲ぼたん','2年','ビューティー科','くらえ、これが100%の力だ','https://makeit.com/s3/photo'),
+('小酒部雄二','2年','ビューティー科','ミミズだって','https://makeit.com/s3/photo'),
+('酒田鐵造','2年','ビューティー科','オケラだって','https://makeit.com/s3/photo'),
+('酒澤五郎','2年','ビューティー科','ハーゲンダッツ3つで100円!?','https://makeit.com/s3/photo')
+;
