c38531d7fb4fb83e38f50bb9f5e3d756ad590867
ymzk-jp <ymzk@gmail.com>
2018-10-03 13:17:21 +0900

    commit もれ!

--- V20181003/05_vote.sql
+++ V20181003/05_vote.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `vote` (
     `voter_id` INT NOT NULL AUTO_INCREMENT COMMENT '投票者ID',
     `product_id` SMALLINT NOT NULL COMMENT 'プロダクトID',
-    `lottery_num` SMALLINT UNIQUE NOT NULL COMMENT '抽選番号',
+    `lottery_num` SMALLINT NOT NULL COMMENT '抽選番号',
     PRIMARY KEY (`voter_id`, `product_id`),
     CONSTRAINT `vote_to_product_id` FOREIGN KEY `vote` (`product_id`) REFERENCES `products` (`id`)
 )DEFAULT CHARSET=utf8mb4 COMMENT='投票一元管理テーブル';
