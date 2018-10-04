db2378565bdf6a39be210e14dcb9995ff17b5600
ymzk-jp <ymzk@gmail.com>
2018-10-04 15:28:24 +0900

    voteテーブル修正

--- V20181003/05_vote.sql
+++ V20181003/05_vote.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS `vote` (
-    `voter_id` INT NOT NULL AUTO_INCREMENT COMMENT '投票者ID',
+    `id` INT NOT NULL AUTO_INCREMENT COMMENT 'レコードを特定するためのもの',
+    `voter_id` VARCHAR(8) NOT NULL COMMENT '投票者ID',
     `product_id` SMALLINT NOT NULL COMMENT 'プロダクトID',
-    `lottery_num` SMALLINT NOT NULL COMMENT '抽選番号',
+    `lottery_num` SMALLINT(4) NOT NULL COMMENT '4ケタの抽選番号',
     PRIMARY KEY (`voter_id`, `product_id`),
     CONSTRAINT `vote_to_product_id` FOREIGN KEY `vote` (`product_id`) REFERENCES `products` (`id`)
 )DEFAULT CHARSET=utf8mb4 COMMENT='投票一元管理テーブル';
