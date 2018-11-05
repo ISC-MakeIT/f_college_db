5351bee6192d83a6c1029abd05bdd4b6a4017a66
ymzk-jp <ymzk@gmail.com>
2018-11-05 23:59:35 +0900

    add products.product_id to manage products

--- V20181101/02_products.sql
+++ V20181101/02_products.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS `products` (
     `id` INT NOT NULL AUTO_INCREMENT COMMENT 'サロゲートキー',
-    `product_id` SMALLINT NOT NULL COMMENT '作品番号',
-    `appearance` SMALLINT COMMENT '出場順',
+    `product_id` INT NOT NULL COMMENT '作品管理番号',
+    `product_number` SMALLINT NOT NULL COMMENT '作品番号（重複あり）',
+    `entry_order` SMALLINT COMMENT '出場順',
     `genre` VARCHAR(30) COMMENT '出場種目',
     `theme` VARCHAR(100) COMMENT '作品名',
     `concept` VARCHAR(250) COMMENT '作品コンセプト',
--- V20181101/04_product_menbers.sql
+++ V20181101/04_product_menbers.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS `product_menbers` (
     `id` INT NOT NULL AUTO_INCREMENT COMMENT 'サロゲートキー',
     `product_id` INT NOT NULL COMMENT '作品番号',
     `student_id` INT NOT NULL COMMENT '学生ID',
-    `leader_flg` BOOLEAN COMMENT '班長=>1',
+    `leader_flg` BOOLEAN COMMENT '班長=>1, それ以外=>0',
     PRIMARY KEY (`id`),
-    CONSTRAINT `product_menbers_to_product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
+    CONSTRAINT `product_menbers_to_product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`),
     CONSTRAINT `product_menbers_to_student_id` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
 )DEFAULT CHARSET=utf8mb4 COMMENT='チーム構成一覧';
