930f9102e2b62e28ddd585a01e70afa07451c6c6
ymzk-jp <ymzk@gmail.com>
2018-11-06 00:53:01 +0900

    add photos table

--- V20181101/02_products.sql
+++ V20181101/02_products.sql
@@ -6,5 +6,6 @@ CREATE TABLE IF NOT EXISTS `products` (
     `genre` VARCHAR(30) COMMENT '出場種目',
     `theme` VARCHAR(100) COMMENT '作品名',
     `concept` VARCHAR(250) COMMENT '作品コンセプト',
-    PRIMARY KEY (`id`)
+    KEY `index_to_product_id` (`id`),
+    PRIMARY KEY (`product_id`)
 )DEFAULT CHARSET=utf8mb4 COMMENT='作品一覧';
