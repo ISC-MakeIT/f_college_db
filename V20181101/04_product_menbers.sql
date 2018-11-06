4cebdc14513d9f7680feefbe72f5c590581d9e75
ymzk-jp <ymzk@gmail.com>
2018-11-07 00:29:03 +0900

    product_menbers.product_number corresponded to products.product_number

--- V20181101/04_product_menbers.sql
+++ V20181101/04_product_menbers.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS `product_menbers` (
     `id` INT NOT NULL AUTO_INCREMENT COMMENT 'サロゲートキー',
-    `product_id` INT NOT NULL COMMENT '作品番号',
+    `product_id` INT NOT NULL COMMENT '作品管理番号',
+    `product_number` INT NOT NULL COMMENT '作品番号（重複あり）',
     `student_id` INT NOT NULL COMMENT '学生ID',
     `leader_flg` BOOLEAN COMMENT '班長=>1, それ以外=>0',
     PRIMARY KEY (`id`),
