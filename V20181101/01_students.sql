a7866538cd51ecdee79913d7cee6a56b34bd04ef
ymzk-jp <ymzk@gmail.com>
2018-11-07 18:49:16 +0900

    fix references

--- V20181101/01_students.sql
+++ V20181101/01_students.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `students` (
     `id` INT NOT NULL AUTO_INCREMENT COMMENT 'サロゲートキー',
-    `student_id` SMALLINT UNIQUE NOT NULL COMMENT '学生ID',
+    `student_id` SMALLINT UNIQUE  NOT NULL COMMENT '学生ID',
     `name` VARCHAR(50) NOT NULL COMMENT '氏名',
     `class` VARCHAR(30) COMMENT '学年学科',
     PRIMARY KEY (`id`)
--- V20181101/all_insert.sql
+++ V20181101/all_insert.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `students` (
     `id` INT NOT NULL AUTO_INCREMENT COMMENT 'サロゲートキー',
-    `student_id` SMALLINT UNIQUE NOT NULL COMMENT '学生ID',
+    `student_id` SMALLINT UNIQUE  NOT NULL COMMENT '学生ID',
     `name` VARCHAR(50) NOT NULL COMMENT '氏名',
     `class` VARCHAR(30) COMMENT '学年学科',
     PRIMARY KEY (`id`)
@@ -9,17 +9,17 @@ CREATE TABLE IF NOT EXISTS `students` (
 
 CREATE TABLE IF NOT EXISTS `products` (
     `id` INT NOT NULL AUTO_INCREMENT COMMENT 'サロゲートキー',
-    `product_id` INT NOT NULL UNIQUE COMMENT '作品管理番号',
+    `product_id` INT UNIQUE NOT NULL COMMENT '作品管理番号',
     `product_number` SMALLINT NOT NULL COMMENT '作品番号（重複あり）',
     `entry_order` SMALLINT COMMENT '出場順',
     `genre` VARCHAR(30) COMMENT '出場種目',
     `theme` VARCHAR(100) COMMENT '作品名',
     `concept` VARCHAR(250) COMMENT '作品コンセプト',
-    `leader_id` SMALLINT NOT NULL COMMENT 'リーダーID',
-    KEY `index_to_product_id` (`id`),
-    PRIMARY KEY (`product_id`),
-    CONSTRAINT `photos_to_leader_id` FOREIGN KEY (`leader_id`) REFERENCES `students` (`student_id`)
+    `leader_id` SMALLINT NOT NULL COMMENT 'プロダクトオーナーID',
+    PRIMARY KEY (`id`),
+    CONSTRAINT `products_to_leader_id` FOREIGN KEY (`leader_id`) REFERENCES `students` (`student_id`)
 )DEFAULT CHARSET=utf8mb4 COMMENT='作品一覧';
+
 
 
 CREATE TABLE IF NOT EXISTS `photos` (
@@ -31,21 +31,24 @@ CREATE TABLE IF NOT EXISTS `photos` (
 )DEFAULT CHARSET=utf8mb4 COMMENT='作品画像一覧';
 
 
-CREATE TABLE IF NOT EXISTS `product_menbers` (
+
+CREATE TABLE IF NOT EXISTS `product_members` (
     `id` INT NOT NULL AUTO_INCREMENT COMMENT 'サロゲートキー',
     `product_id` INT NOT NULL COMMENT '作品管理番号',
     `product_number` INT NOT NULL COMMENT '作品番号（重複あり）',
-    `student_id` INT NOT NULL COMMENT '学生ID',
+    `student_id` SMALLINT NOT NULL COMMENT '学生ID',
     `leader_flg` BOOLEAN COMMENT '班長=>1, それ以外=>0',
     PRIMARY KEY (`id`),
-    CONSTRAINT `product_menbers_to_product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`),
-    CONSTRAINT `product_menbers_to_student_id` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
+    CONSTRAINT `product_members_to_product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`),
+    CONSTRAINT `product_members_to_student_id` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`) ON UPDATE CASCADE ON DELETE CASCADE
 )DEFAULT CHARSET=utf8mb4 COMMENT='チーム構成一覧';
+
+
 
 CREATE TABLE IF NOT EXISTS `vote` (
     `id` INT NOT NULL AUTO_INCREMENT COMMENT 'サロゲートキー',
     `voter_id` VARCHAR(16) NOT NULL COMMENT '投票者ID',
-    `product_id` INT NOT NULL COMMENT '作品管理番号',
+    `product_id` INT NOT NULL COMMENT 'プロダクトID',
     `lottery_num` SMALLINT(4) NOT NULL COMMENT '4ケタの抽選番号',
     PRIMARY KEY (`id`),
     CONSTRAINT `vote_to_product_id` FOREIGN KEY `vote` (`product_id`) REFERENCES `products` (`product_id`)
