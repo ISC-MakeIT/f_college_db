6d28b7f025438e0a0281a36b80d699fd36d3905c
ymzk-jp <ymzk@gmail.com>
2018-11-14 18:22:10 +0900

    vote re make

--- V20181101/05_vote.sql
+++ V20181101/05_vote.sql
@@ -1,8 +1,7 @@
 CREATE TABLE IF NOT EXISTS `vote` (
     `id` INT NOT NULL AUTO_INCREMENT COMMENT 'サロゲートキー',
-    `voter_id` VARCHAR(16) NOT NULL COMMENT '投票者ID',
-    `product_id` INT NOT NULL COMMENT 'プロダクトID',
-    `lottery_num` SMALLINT(4) NOT NULL COMMENT '4ケタの抽選番号',
+    `product_id` INT NOT NULL COMMENT '作品管理番号',
+    `number_of_votes` INT COMMENT '作品ごとの集計',
     PRIMARY KEY (`id`),
     CONSTRAINT `vote_to_product_id` FOREIGN KEY `vote` (`product_id`) REFERENCES `products` (`product_id`)
 )DEFAULT CHARSET=utf8mb4 COMMENT='投票一元管理テーブル';
